065a5bdac4a860928b498089ff82cb286e7b34c0
admsmn02 <adam.simon@edu.devinci.fr>
2024-02-27 15:44:00 +0100

    fake db

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,14 @@
 const products = [
-    { barcode: '0613008730772', name: 'Product  1', price: 10.99, description: 'Bouteille test' },
-    { barcode: '234567890123', name: 'Product  2', price: 15.99, description: 'This is product  2' },
-    { barcode: '345678901234', name: 'Product  3', price: 20.99, description: 'This is product  3' },
-    { barcode: '456789012345', name: 'Product  4', price: 25.99, description: 'This is product  4' },
-    { barcode: '567890123456', name: 'Product  5', price: 30.99, description: 'This is product  5' },
-    { barcode: '678901234567', name: 'Product  6', price: 35.99, description: 'This is product  6' },
-    { barcode: '789012345678', name: 'Product  7', price: 40.99, description: 'This is product  7' },
-    { barcode: '890123456789', name: 'Product  8', price: 45.99, description: 'This is product  8' },
-    { barcode: '901234567890', name: 'Product  9', price: 50.99, description: 'This is product  9' },
-    { barcode: '012345678901', name: 'Product  10', price: 55.99, description: 'This is product  10' }
+    { barcode: '0613008730772', name: 'Bouteille Arizona', reduction: 10, description: 'Bouteille test' },
+    { barcode: '8002270116544', name: 'Bouteille San Pellegrino', reduction: 5, description: 'This is product  2' },
+    { barcode: '3268840001008', name: 'Bouteille Cristaline', reduction: 8, description: 'This is product  3' },
+    { barcode: '3700123300380', name: 'Nestle Pure Life', reduction: 3, description: 'This is product  4' },
+    { barcode: '567890123456', name: 'Product  5', reduction: 30.99, description: 'This is product  5' },
+    { barcode: '678901234567', name: 'Product  6', reduction: 35.99, description: 'This is product  6' },
+    { barcode: '789012345678', name: 'Product  7', reduction: 40.99, description: 'This is product  7' },
+    { barcode: '890123456789', name: 'Product  8', reduction: 45.99, description: 'This is product  8' },
+    { barcode: '901234567890', name: 'Product  9', reduction: 50.99, description: 'This is product  9' },
+    { barcode: '012345678901', name: 'Product  10', reduction: 55.99, description: 'This is product  10' }
 ];
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
             // if the code is in the products array
             var product = products.find(p => p.barcode === code);
             if (product) {
-                alert('Product find, ' + product.name + ' ' + product.price + ' ' + product.description)
+                alert('Reduction Code Found on ' + product.name + '. The reduction is ' + product.reduction + '%');
             } else {
                 alert('This barcode is not available');
             }
